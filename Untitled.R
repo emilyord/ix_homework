16bd51f6ac6aec85571a0b58bc3bd4892098df63
Emily Ord <emilyord@Emilys-MacBook-Pro-2.local>
2019-07-02 12:49:51 +0200

    r file

---
+++ Untitled.R
@@ -0,0 +1,19 @@
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy)) +
+  facet_grid(drv~.)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy)) +
+  facet_grid(.~cyl)
+
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x=displ, y=hwy))+
+  facet_wrap(~class, nrow = 2)
+
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x=displ, y=hwy))+
+  geom_smooth(mapping = aes(x=displ, y=hwy))
+  
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x=displ, y=hwy))+
+  geom_smooth(mapping = aes(x=displ, y=hwy, color=drv))
